947228a61b9da720a5212fed5250e91ba965efd5
omdwivedi <36497497+omdwivedi@users.noreply.github.com>
2020-07-07 21:01:30 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,16 @@
+#square formation using turtle module#
+import turtle
+Om_turtle=turtle.Turtle()
+def square():
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+
+square()
+Om_turtle.forward(140)
+
+square()
