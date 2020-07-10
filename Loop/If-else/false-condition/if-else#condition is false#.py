6cfd35d4984f7a5f51d70a921edf9a7e52769c90
Om Dwivedi <36497497+omdwivedi@users.noreply.github.com>
2020-07-11 02:26:30 +0530

    Add files via upload

---
+++ Loop/If-else/false-condition/if-else#condition is false#.py
@@ -0,0 +1,22 @@
+
+#if-else condition checking using square function task# 
+import turtle
+Om_turtle=turtle.Turtle()
+def square():
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+
+Elephant_weight=1000
+Ant_weight=0.15
+#applying If else loop  #
+
+#applying when condition is false #
+if Elephant_weight<Ant_weight:
+  square()
+else:
+  Om_turtle.forward(100)
