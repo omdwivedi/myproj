81fcb47388f8a4f8f1eba4e0a1b6681e391d7485
Om Dwivedi <36497497+omdwivedi@users.noreply.github.com>
2020-07-11 02:14:50 +0530

    Add files via upload

---
+++ while/false/while-false.py
@@ -0,0 +1,21 @@
+#while condition checking using square function task# 
+import turtle
+Om_turtle=turtle.Turtle()
+def square():
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+  Om_turtle.right(90)
+  Om_turtle.forward(100)
+
+Elephant_weight=1000
+Ant_weight=0.15
+#applying while loop  #
+
+#applying when condition is false #
+while Elephant_weight<Ant_weight:
+  square()
+else:
+  Om_turtle.forward(100)
